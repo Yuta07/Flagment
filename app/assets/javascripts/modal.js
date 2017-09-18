7467a3cc2b0b84b30ab4e0a44fb4fd6af82d5cbd
atm0015 <atm.intern2015+atm0015@gmail.com>
2017-09-18 18:29:06 +0900

    add modal-window

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -11,4 +11,29 @@
 // about supported directives.
 //
 //= require turbolinks
-// require_tree .
+//= require_tree .
+
+(function() {
+  'use strict';
+
+  var open = document.getElementById('open');
+  var close = document.getElementById('close');
+  var modal = document.getElementById('modal');
+  var mask = document.getElementById('mask');
+
+  open.addEventListener('click', function() {
+    modal.className = '';
+    mask.className = '';
+  });
+
+  close.addEventListener('click', function() {
+    modal.className = 'hidden';
+    mask.className = 'hidden';
+  });
+
+  mask.addEventListener('click', function() {
+    // modal.className = 'hidden';
+    // mask.className = 'hidden';
+    close.click();
+  });
+})();
