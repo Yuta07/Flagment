3d1e9530fc85f15423bd533c0a8b8240c384c6f9
atm0015 <atm.intern2015+atm0015@gmail.com>
2017-09-25 16:58:19 +0900

    add signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,36 @@
 class UsersController < ApplicationController
   def new
+    @user=User.new
   end
 
   def edit
+
   end
 
   def show
+    @user=User.find_by(params[:id])
   end
 
   def index
+    @user=User.all(params[:id])
   end
+
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      flash[:success] = "Created new account"
+      redirect_to @user
+    else
+      flash[:error] = "Account creation failure"
+      render 'home/index'
+    end
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
 
+  validates :name, presence:true, length:{maximum: 50},
+            uniqueness:{case_sensitive: true}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  validates :email, presence:true, length:{maximum: 255},
+            format:{ with: VALID_EMAIL_REGEX }, uniqueness:{case_sensitive: true}
+
+  has_secure_password
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,20}+\z/
+  validates :password, presence:true, length:{in: 8..20},
+            format:{ with: VALID_PASSWORD_REGEX }
 
 end
